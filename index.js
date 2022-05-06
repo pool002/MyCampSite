0d140c36d149686e8be4649de13941afd28d9ef4
Pool <pool@pool.com>
2022-05-06 14:25:36 +0530

    added secure cookie

--- index.js
+++ index.js
@@ -50,8 +50,7 @@ const sessionConfig = {                                     //Configuration for 
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
